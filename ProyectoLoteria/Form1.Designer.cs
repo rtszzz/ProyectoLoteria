b9a3428b0263e55d5dbe3a8f4e614a2f77a8637f
RAMIRO TORRES SUAREZ <I21050355@monclova.tecnm.mx>
2021-12-09 04:54:17 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoLoteria/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Loteria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbcarta = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBuenas = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcarta)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbcarta);
+            this.groupBox1.Location = new System.Drawing.Point(795, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 361);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "carta";
+            // 
+            // pbcarta
+            // 
+            this.pbcarta.Location = new System.Drawing.Point(6, 21);
+            this.pbcarta.Name = "pbcarta";
+            this.pbcarta.Size = new System.Drawing.Size(236, 334);
+            this.pbcarta.TabIndex = 0;
+            this.pbcarta.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(1189, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(249, 361);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Historial";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 21);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(236, 334);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(1050, 174);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(133, 94);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Restan";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(7, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 75);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(795, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 48);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Siguiente Carta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1189, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(249, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Barajear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnBuenas
+            // 
+            this.btnBuenas.Location = new System.Drawing.Point(597, 507);
+            this.btnBuenas.Name = "btnBuenas";
+            this.btnBuenas.Size = new System.Drawing.Size(153, 53);
+            this.btnBuenas.TabIndex = 5;
+            this.btnBuenas.Text = "BUENAS!";
+            this.btnBuenas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1450, 645);
+            this.Controls.Add(this.btnBuenas);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbcarta)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbcarta;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBuenas;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ ProyectoLoteria/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loteria
+{
+    public partial class Form1 : Form
+    {
+        Stack<int> carta = new Stack<int>();
+        const int CANTIDAD_CARTAS = 54;
+        private PictureBox[] cartas;
+        private PictureBox[] tabla;
+       
+        Random rnd1 = new Random();
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cartas = new PictureBox[CANTIDAD_CARTAS];
+            tabla = new PictureBox[25];
+            inicializarTabla();
+        }
+
+        private void inicializarTabla()
+        {
+            int r = 0, c = 0;
+
+            int[] cartas = new int[34];
+
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = i + 1;
+            }
+
+            Random rnd = new Random();
+            int a, aux;
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = rnd.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+            }
+
+
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = new PictureBox();
+                tabla[i].Location = new System.Drawing.Point(20 + (c * 90), 25 + (r * 130));
+                tabla[i].Name = "picTabla1" + 1;
+                tabla[i].Size = new System.Drawing.Size(85, 125);
+                tabla[i].TabIndex = 0 + i;
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].TabStop = false;
+                tabla[i].Image = Image.FromFile(@"C:\Users\usuario\source\repos\ProyectoLoteria\ProyectoLoteria\bin\Debug" + (cartas[i]) + ".jpg");
+                this.Controls.Add(tabla[i]);
+                c++;
+                if (c == 5)
+                {
+                    r++;
+                    c = 0;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listView1.View = View.LargeIcon;
+            this.imageList1.ImageSize = new Size(150, 220);
+            this.listView1.LargeImageList = this.imageList1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (54 - carta.Count()).ToString();
+            bool bandera = false;
+            if (carta.Count() == 54)
+            {
+                bandera = true;
+                MessageBox.Show("son todas las cartas", "AVIZO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            while (!bandera)
+            {
+                int num = rnd1.Next(1, 54);
+
+                if (!carta.Contains(num))
+                {
+                    pbcarta.Image = Image.FromFile(@"C:\Users\usuario\source\repos\ProyectoLoteria\ProyectoLoteria\bin\Debug" + num + ".jpg");
+                    pbcarta.SizeMode = PictureBoxSizeMode.StretchImage;
+                    carta.Push(num);
+
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = i;
+                    this.listView1.Items.Add(item);
+                    this.imageList1.Images.Add(Image.FromFile(@"C:\Users\usuario\source\repos\ProyectoLoteria\ProyectoLoteria\bin\Debug" + num + ".jpg"));
+                    bandera = true;
+                    i++;
+                }
+            }
+        }
+
+        private void btnBuenas_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Voz 004.wav");
+            player.Play();
+        }
+    }
+}
